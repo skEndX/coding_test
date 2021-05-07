9b20690797a9b57630ce39885bf535ff5ccc49b4
skEndX <gmlek1054@inu.ac.kr>
2021-05-07 15:16:38 +0900

    update

--- DFS_BFS.py
+++ DFS_BFS.py
@@ -101,3 +101,94 @@ graph[2].append((0, 5))
 
 print(graph)
 '''
+
+'''
+# DFS
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False]*9
+
+dfs(graph, 1, visited)
+'''
+
+'''
+# BFS
+from collections import deque
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False]*9
+
+bfs(graph, 1, visited)
+'''
+
+'''
+# 음료수 얼려 먹기
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x+1, y)
+        dfs(x, y+1)
+        dfs(x-1, y)
+        dfs(x, y-1)
+        return True
+    return False
+
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
+'''
